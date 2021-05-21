c6026625b241681c3e8fef33324e70c6efabb37c
lucie.bellenger <73110649+Lanuitjedanse@users.noreply.github.com>
2021-05-21 08:10:42 +0200

    new project

--- my-app/src/Projects.js
+++ my-app/src/Projects.js
@@ -1,35 +1,26 @@
 import "./Projects.css";
 import React from "react";
-// import ReactDOM from "react-dom";
-// import GifPlayer from "react-gif-player";
-// import playButton from "./play-button.svg";
-
-// import MyGifWrapper from "./MyGifWrapper";
-
-// import GifPlayer from "./GifPlayer";
-// import { useEffect, useState } from "react";
 
 import { ReactComponent as Next } from "./next.svg";
 
-// import { Link } from "react-router-dom";
 export default function Projects() {
-  // const [play, setPlay] = useState(false);
-  // // const [buttonVisible, setButtonVisible] = useState(true);
-
-  // // const pause = () => {
-  // //     setPlay(!play);
-  // // };
-  // console.log("setPlay: ", setPlay);
-  // const playOrPause = (e) => {
-  //     setPlay(!play);
-  // };
   const projects = [
     {
       id: 0,
+      name: "thing Online",
+      description:
+        "My first profesionnal project working as a Frontend Developer. thing Online aims to revolutionise virtual workshops. This project uses stack such as Node.js, Nuxt.js, Vue.js, Vuex, socket.io, JavaScript, TypeScript, Python, MongoDB, LiveSwitch, PixiJS, and more",
+      url: "https://app.thing.online/login",
+      urlBackground: "/background7.jpg",
+      css: "color9",
+      still: "/thing-online.png",
+      position: "box-right",
+    },
+    {
+      id: 1,
       name: "Lanuitjedanse",
       description:
-        "My final fullstack project during the bootcamp at Spiced Academy. Lanuitjedanse allows you to find a bar or a club depending on your music taste. This project was done in 1 week using Node.js, React and Postgres.",
-      image: "/lanuitjedanse.gif",
+        "My final fullstack project during the bootcamp at Spiced Academy. Lanuitjedanse allows you to find a bar or a club depending on your music taste. This project was done using Node.js, React, PostgresSQL and Google Maps API.",
       url: "https://lanuitjedanse.herokuapp.com/",
       urlBackground: "/background1.jpg",
       css: "color1",
@@ -37,22 +28,20 @@ export default function Projects() {
       position: "box-left",
     },
     {
-      id: 1,
+      id: 2,
       name: "Tracklist",
       description:
-        "My third fullstack project at Spiced Academy, this project was done in 2 weeks and half, using Node.js, React and Postgres. It is a social media platform for music lovers.",
-      image: "/tracklist-social.gif",
+        "My third fullstack project at Spiced Academy using Node.js, React and Postgres. It is a social media platform for music lovers.",
       url: "https://social-tracklist.herokuapp.com/",
       urlBackground: "/background4.jpg",
       css: "color2",
       still: "/tracklist.png",
     },
     {
-      id: 2,
+      id: 3,
       name: "Greenstagram",
       description:
-        "My second fullstack project, this project was done in 1 week, using Node.js, Vue.js and Postgres. It is a social media platform similar to instagram for plant lovers",
-      image: "/greenstagram.gif",
+        "My second fullstack project, this project was done using Node.js, Vue.js and Postgres. It is a social media platform similar to instagram for plant lovers",
       url: "https://lucie-greenstagram.herokuapp.com/",
       urlBackground: "/background9.jpg",
       css: "color6",
@@ -60,11 +49,10 @@ export default function Projects() {
       position: "box-left",
     },
     {
-      id: 3,
+      id: 4,
       name: "Petition for Cheese Rights",
       description:
-        "My first fullstack project at Spiced Academy. This project was done in 2 weeks using Node.js, Handlebars and Postgres. This is a petition website for cheese rights :) ",
-      image: "/petition.gif",
+        "My first fullstack project at Spiced Academy. This project was done using Node.js, Handlebars and Postgres. This is a petition website for cheese rights :) ",
       url: "https://stormy-chamber-44911.herokuapp.com/register",
       urlBackground: "/background3.jpg",
       css: "color3",
@@ -72,11 +60,10 @@ export default function Projects() {
       position: "box-right",
     },
     {
-      id: 4,
+      id: 5,
       name: "Soundify",
       description:
         "Soundify is a platform to search for music on Spotify. JQuery and Spotify' API.",
-      image: "/soundify.gif",
       url: "",
       urlBackground: "/background5.jpg",
       css: "color4",
@@ -84,10 +71,9 @@ export default function Projects() {
       position: "box-left",
     },
     {
-      id: 5,
+      id: 6,
       name: "Connect Four Special Ru Paul's Drag Race Edition",
       description: "Connect four project done with JQuery.",
-      image: "/dragrace.gif",
       url: "",
       urlBackground: "/background6.jpg",
       css: "color5",
