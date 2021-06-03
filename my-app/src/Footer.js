61e79034e3c880506ea9d8e2af6eda04c57e1ac7
lucie.bellenger <73110649+Lanuitjedanse@users.noreply.github.com>
2021-06-03 12:30:51 +0200

    style: new icons and fix github icon

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,46 +1,56 @@
 import "./Footer.css";
 
-import { ReactComponent as Email } from "./email.svg";
-import { ReactComponent as Github } from "./github.svg";
-import { ReactComponent as LinkedIn } from "./linkedin.svg";
+import { ReactComponent as Email } from "./paper-plane-solid.svg";
+import { ReactComponent as Github } from "./github-brands.svg";
+import { ReactComponent as GitLab } from "./gitlab-brands.svg";
+
+import { ReactComponent as LinkedIn } from "./linkedin-in-brands.svg";
 // import { useState } from "react";
 
 export default function Footer() {
-    return (
-        <footer>
-            <div className="box-footer">
-                <div className="row">
-                    <Email className="menu-icon" />
-                    <a
-                        className="email"
-                        href="mailto:bellenger.lucie@gmail.com"
-                    >
-                        bellenger.lucie@gmail.com
-                    </a>
-                </div>
-                <div className="row">
-                    <Github className="menu-icon" />
-                    <a
-                        className="email"
-                        href="https://github.com/Lanuitjedanse"
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        GitHub
-                    </a>
-                </div>
-                <div className="row">
-                    <LinkedIn className="menu-icon" />
-                    <a
-                        className="email"
-                        href="https://www.linkedin.com/in/lucie-bellenger-890b9860/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        LinkedIn
-                    </a>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="box-footer">
+        <div className="row">
+          <Email className="menu-icon color-icon" />
+          <a className="email" href="mailto:bellenger.lucie@gmail.com">
+            bellenger.lucie@gmail.com
+          </a>
+        </div>
+        <div className="row">
+          <Github className="menu-icon color-icon" />
+          <a
+            className="email"
+            href="https://github.com/Lanuitjedanse"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </div>
+        <div className="row">
+          <GitLab className="menu-icon color-icon" />
+          <a
+            className="email"
+            href="https://www.linkedin.com/in/lucie-bellenger-890b9860/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitLab
+          </a>
+        </div>
+        <div className="row">
+          <LinkedIn className="menu-icon color-icon" />
+          <a
+            className="email"
+            href="https://www.linkedin.com/in/lucie-bellenger-890b9860/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
